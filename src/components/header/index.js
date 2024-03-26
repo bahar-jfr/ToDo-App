f88405e5df4b57f0ebcf3eeefae9d14a317c6db9
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-03-26 14:22:02 +0330

    feat: header's element added

---
+++ src/components/header/index.js
@@ -0,0 +1,34 @@
+import { El, icon } from "../../library";
+
+export function Header() {
+  const header =  El({
+    element: "header",
+    className: "bg-dark_purple w-full flex gap-3 items-center p-6 text-white",
+    children: [
+      icon("fa-solid fa-list-ul"),
+      El({
+        element: "p",
+        className: "flex-1 text-xs sm:text-sm md:text-xl",
+        children: "My To-Do Task",
+      }),
+      El({
+        element: "div",
+        className: "bg-light_purple px-4 py-2 rounded-sm",
+        children: [
+          icon("fa-solid fa-magnifying-glass pr-4"),
+          El({
+            element: "input",
+            className: "bg-transparent outline-none w-24 md:w-56",
+            type: "text",
+            placeholder: "Search",
+          }),
+        ],
+      }),
+      icon("fa-solid fa-filter fa-xl mx-3"),
+      icon("fa-solid fa-square-plus fa-xl"),
+    ],
+  });
+  header.children[4].addEventListener("click",()=> showModal())
+
+  return header
+}
