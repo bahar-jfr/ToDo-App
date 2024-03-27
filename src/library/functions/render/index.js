69180b0347cf1eac0c36eabb335dfaae1221606b
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-03-27 17:15:22 +0330

    feat: create render function to append tr, td to table

---
+++ src/library/functions/render/index.js
@@ -0,0 +1,51 @@
+import { El } from "../../helpers";
+import { actionBtn } from "../../helpers/actions";
+
+export function render(tasks) {
+  const tableBody = document.getElementById("tableBody");
+
+  // Reset the table bode
+  tableBody.innerHTML = "";
+
+  tasks.forEach((item) => {
+    const row = El({
+      element: "tr",
+      id: item.id,
+      className: "border-collapse border-2",
+      children: [
+        El({
+          element: "td",
+          className :" py-3 w-1/5 text-center",
+          children: item.taskName,
+        }),
+        El({
+            element :"td",
+            className: " border-x-2  py-3 w-1/5 text-center",
+            children:["hhh"]
+        }),
+        El({
+            element :"td",
+            className: " border-x-2  py-3 w-1/5 text-center",
+            children:["jhh"]
+        }),
+        El({
+          element: "td",
+          className: " border-x-2  py-3 w-1/5 text-center",
+          children: [
+            El({
+              element: "p",
+              className: "border-2 border-blue rounded-2xl w-32 md:ml-20",
+              children: item.date,
+            }),
+          ],
+        }),
+        El({
+          element: "td",
+          className :"flex justify-center gap-2 py-3",
+          children: [actionBtn(0), actionBtn(1), actionBtn(2)],
+        }),
+      ],
+    });
+    tableBody.append(row);
+  });
+}
