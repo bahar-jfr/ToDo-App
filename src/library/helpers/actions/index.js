8debfb5b91984df3bb0e746a01a6f364a884f877
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-03-27 16:22:01 +0330

    feat: create action data, use to make it's buttons

---
+++ src/library/helpers/actions/index.js
@@ -0,0 +1,31 @@
+import { El } from "../El";
+const actionData = [
+  {
+    name: "delete",
+    src: "../../../assets/images/ion--trash-sharp.svg",
+    class: "bg-light_pink w-fit md:px-2  py-1  rounded-md",
+  },
+  {
+    name: "edit",
+    src: "../../../assets/images/mdi--edit.svg",
+    class: "bg-light_blue w-fit md:px-2  py-1  rounded-md",
+  },
+  {
+    name: "view",
+    src: "../../../assets/images/ion--eye.svg",
+    class: "bg-dark_gray w-fit md:px-2  py-1  rounded-md",
+  },
+];
+
+export function actionBtn(index) {
+  return El({
+    element: "div",
+    className: `${actionData[index].class}`,
+    children: [
+      El({
+        element: "img",
+        src: actionData[index].src,
+      }),
+    ],
+  });
+}
