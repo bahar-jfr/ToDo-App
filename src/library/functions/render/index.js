fe550cda78144f582a01b34026be80395a906076
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-03-29 14:24:34 +0330

    feat: create func to make status and priority btn

--- src/library/functions/render/index.js
+++ src/library/functions/render/index.js
@@ -1,5 +1,6 @@
 import { El } from "../../helpers";
 import { actionBtn } from "../../helpers/actions";
+import { statusBtn } from "../../helpers/status";
 
 export function render(tasks) {
   const tableBody = document.getElementById("tableBody");
@@ -15,19 +16,12 @@ export function render(tasks) {
       children: [
         El({
           element: "td",
-          className :" py-3 w-1/5 text-center",
+          className: " py-3 w-1/5 text-center",
           children: item.taskName,
         }),
-        El({
-            element :"td",
-            className: " border-x-2  py-3 w-1/5 text-center",
-            children:["hhh"]
-        }),
-        El({
-            element :"td",
-            className: " border-x-2  py-3 w-1/5 text-center",
-            children:["jhh"]
-        }),
+        statusBtn(item.priority),
+        statusBtn(item.status),
+
         El({
           element: "td",
           className: " border-x-2  py-3 w-1/5 text-center",
@@ -41,7 +35,7 @@ export function render(tasks) {
         }),
         El({
           element: "td",
-          className :"flex justify-center gap-2 py-3",
+          className: "flex justify-center gap-2 py-3",
           children: [actionBtn(0), actionBtn(1), actionBtn(2)],
         }),
       ],
---
+++ src/library/helpers/status/index.js
@@ -0,0 +1,31 @@
+import { El } from "../El";
+
+const statusData = {
+  Low: "bg-light_gray",
+  High: "bg-light_pink text-white",
+  Medium: "bg-dark_yellow",
+  Todo: "bg-light_pink text-white",
+  Doing: "bg-dark_yellow",
+  Done: "bg-dark_green",
+};
+
+export function statusBtn(item) {
+  return El({
+    element: "td",
+    className: " border-x-2  py-3 w-1/5 text-center ",
+    children: [
+        El({
+            element: "div",
+            className: "w-full flex justify-center",
+            children:[
+                El({
+                    element: "p",
+                    className: `font-semibold md:py-1 md:px-2 px-1 w-fit rounded-3xl ${statusData[item]} `,
+                    innerText: item,
+                  }),
+            ]
+        })
+
+    ],
+  });
+}
