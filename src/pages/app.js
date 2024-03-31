4f8f5354f51bb90fcf69aab27a3b8de0c8defbb2
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-03-31 14:15:45 +0330

    fix: render data when page load or refresh

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,13 +1,19 @@
+import { getData } from "../api/getData";
 import { Footer } from "../components";
 import { Container } from "../components/container";
 import { Header } from "../components/header";
 import { Main } from "../components/main";
 import { El } from "../library";
+import { page } from "../library/functions/handelSubmit";
+import { render } from "../library/functions/render";
 
 export const App = () => {
+   getData(page.currentPage, page.perPge).then((res) => {
+    render(res.data.data);
+  });
   const app = El({
     element: "div",
     children: [Header(), Main(), Footer()],
   });
-  return Container(app)
+  return Container(app);
 };
