27b6434c046ee5b5233e449f27b6904a52427d1c
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-01 13:02:16 +0330

    feat: add filter and view modal

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./src/style/index.css";
 import { App } from "./src/pages/app.js";
-import { Modal, Overlay } from "./src/components/modal/index.js";
+import { Modal, Overlay, viewModal } from "./src/components/modal/index.js";
+import { Filter } from "./src/components/filter/index.js";
 
 const root = document.getElementById("app");
-root.append(App(), Modal(), Overlay());
+root.append(App(), Modal(), Overlay(), Filter(), viewModal());
