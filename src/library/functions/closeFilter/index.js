a44ebfafab4455c7f0062e80d490fe0abf197e23
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-04 17:06:43 +0330

    fix: loading page

--- src/library/functions/closeFilter/index.js
+++ src/library/functions/closeFilter/index.js
@@ -2,9 +2,8 @@ export function closeFilter() {
   const filter = document.getElementById("filter");
   const overlay = document.getElementById("overlay");
 
-  setTimeout(() => {
+
     filter.className =
       "absolute p-4 flex flex-col gap-4 bg-white h-screen top-0 -right-96 w-96 transition-all duration-1000 invisible hidden ";
     overlay.style.display = "none";
-  }, 1000);
-}
+  }
--- src/library/functions/handelSearch/index.js
+++ src/library/functions/handelSearch/index.js
@@ -1,9 +1,7 @@
 import { getData } from "../../../api/getData";
 import { debounce } from "../../helpers/debounce";
 import { page } from "../handelSubmit";
-import { hideLoading } from "../hideLoading";
 import { render } from "../render";
-import { showLoading } from "../showLoading";
 
 export function handelSearch(e) {
   const searchItem = e.target.value;
